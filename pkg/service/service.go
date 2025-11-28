8a16951aa2a10277a85c797d57d3f97ddf11303a
Karim Shakirov <karimshak06@gmail.com>
2025-11-28 15:40:02 +0300

    chore: project layout & gen proto code
    Signed-off-by: Karim Shakirov <karimshak06@gmail.com>

---
+++ pkg/service/service.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+	"context"
+	"errors"
+)
+
+// Tут вы пишете код.
+// Обратите внимание - в этом задании запрещены глобальные переменные.
+// Если хочется, то для красоты можно разнести логику по разным файлам
+
+func StartMyMicroservice(ctx context.Context, addr string, acl string) error {
+	return errors.New("StartMyMicroservice not implemented yet")
+}
